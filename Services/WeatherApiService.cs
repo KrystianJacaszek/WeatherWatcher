0d63d10ded32644b621bfaad31a974298d4f9abb
KrystianJacaszek <krystian.jacaszek@gmail.com>
2021-01-29 16:34:49 +0100

    Empty project for developmnet

---
+++ Services/WeatherApiService.cs
@@ -0,0 +1,15 @@
+﻿using API.Interfaces;
+using System.Net.Http;
+
+namespace WeatherWatcher.Services
+{
+    public class WeatherApiService : IWeatherApiService
+    {
+        private readonly HttpClient _httpClient;
+        private string _apiKey = "";
+        public WeatherApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
