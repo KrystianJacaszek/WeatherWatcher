0917902da2645b2ccaafc06281591fe28e15f48d
KrystianJacaszek <krystian.jacaszek@gmail.com>
2021-02-14 22:26:35 +0100

    Update Controller to use cache service

--- Controllers/WeatherWatcherController.cs
+++ Controllers/WeatherWatcherController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Interfaces;
 using API.Models;
+using API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,7 @@ namespace API.Controllers
     {
         private readonly IWeatherApiService _weatherApiService;
         private readonly ICacheService _cacheService;
+        private readonly int _tempCityId = 748230;
 
         public WeatherWatcherController(IWeatherApiService weatherApiService, ICacheService cacheService)
         {
@@ -23,34 +25,31 @@ namespace API.Controllers
         [HttpGet("currentweather")]
         public async Task<CurrentWeather> GetCurrentWeatherAsync()
         {
-            var x = await _cacheService.GetCachedCurrentWeatherAsync(1);
+            var x = await _cacheService.GetCachedCurrentWeatherAsync(_tempCityId);
             return x;
         }
 
         [HttpGet("complexcurrentweather")]
         public async Task<ComplexCurrentWeather> GetComplexCurrentWeatherAsync()
         {
-            var x = await _cacheService.GetCachedComplexCurrentWeatherAsync(1);
-            return x;
-        }
-
-        [HttpGet("airpollution")]
-        public async Task<AirPollutionJson> GetAirPolluition()
-        {
-            var lon = 33.441792;
-            var lat = -94.037689;
-            var startDate = DateTime.Now;
-            var endDate = DateTime.Now.AddDays(1);
-            var x = await _weatherApiService.GetAirPollutionnAsync(lon, lat, startDate, endDate);
+            var x = await _cacheService.GetCachedComplexCurrentWeatherAsync(_tempCityId);
             return x;
         }
 
         [HttpGet("forecast")]
-        public async Task<MainForecastJson> GetWeatherForecast()
+        public async Task<IList<ComplexCurrentWeather>> GetWeatherForecast()
         {
-            var x = await _weatherApiService.GetDailyForecastAsync();
+            var x = await _cacheService.GetCachedForecastAsync(_tempCityId);
             return x;
         }
+
+        [HttpGet("airpollution")]
+        public async Task<IList<AirPollutionComponents>> GetAirPolluition()
+        {
+            var x = await _cacheService.GetCachedAirPollutionWeatherAsync(_tempCityId);
+            return x;
+        }
+
 
         [HttpGet("alerts")]
         public async Task<WeatherAlertsJson> GetWeatherAlertst()
@@ -60,5 +59,16 @@ namespace API.Controllers
             var x = await _weatherApiService.GetWeatherAlertsAsync(lon, lat);
             return x;
         }
+
+        //[HttpGet("cities")]
+        //public async Task<IList<CityEntityJson> GetCityLists()
+        //{
+        //    return _cacheService.GetCityList();
+
+        //    //var lon = 50;
+        //    //var lat = 50;
+        //    //var x = await _weatherApiService.GetWeatherAlertsAsync(lon, lat);
+        //    //return x;
+        //}
     }
 }
