b5326ddd64307de6edd2050e3372bb7a7348b9ec
KrystianJacaszek <krystian.jacaszek@gmail.com>
2021-02-28 00:49:25 +0100

    Fix icons

--- weather-watcher-web-app/src/components/ImageFromIconName.tsx
+++ weather-watcher-web-app/src/components/ImageFromIconName.tsx
@@ -4,9 +4,9 @@ import { Image } from "antd";
 export const ImageFromIconName: React.FC<{icon: string}> = ({icon}) =>{
     return(
         <>
-            <Image
-                width={200}
-                src={`../icons/${icon}.png`}
+            <img
+                width={100}
+                src={`/icons/${icon}.png`}
             />
         </>
     )
